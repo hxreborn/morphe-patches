4407f5a57396e8cf21bfdbf8b250e3c85122c125
hxreborn <hxreborn@duck.com>
2026-07-18 09:38:54 +0200

    chore(Forus - Unlock premium): restore attribution
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- patches/src/main/kotlin/app/morphe/patches/forus/misc/premium/UnlockPremiumPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/forus/misc/premium/UnlockPremiumPatch.kt
@@ -5,6 +5,11 @@
  * Additional terms under GPLv3 section 7:
  * - You must preserve reasonable legal notices and author attributions in this file.
  * - Modified versions must not misrepresent the origin of this file.
+ *
+ * Ported from hxreborn/revanced-patches:
+ * https://gitlab.com/hxreborn/revanced-patches/-/commit/2e1e79aff3c4d09d8264ca418ca684e32063552e
+ * Commit 2e1e79aff3c4d09d8264ca418ca684e32063552e (2026-07-16),
+ * patches/src/main/kotlin/app/revanced/patches/forus/misc/premium/UnlockPremiumPatch.kt
  */
 package app.morphe.patches.forus.misc.premium
 
