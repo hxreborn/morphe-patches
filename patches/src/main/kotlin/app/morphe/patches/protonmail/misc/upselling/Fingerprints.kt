4b15bc92367f7de7c4082a1558981dd3bb4aefc8
hxreborn <hxreborn@duck.com>
2026-07-30 16:38:30 +0200

    feat(Proton Mail - Hide upgrade upselling): hide top-bar button and promotional sidebar rows
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- patches/src/main/kotlin/app/morphe/patches/protonmail/misc/upselling/Fingerprints.kt
+++ patches/src/main/kotlin/app/morphe/patches/protonmail/misc/upselling/Fingerprints.kt
@@ -5,7 +5,29 @@
 package app.morphe.patches.protonmail.misc.upselling
 
 import app.morphe.patcher.Fingerprint
+import app.morphe.patches.all.misc.resources.ResourceType
+import app.morphe.patches.all.misc.resources.resourceLiteral
 
-internal object SidebarUpsellingFeatureFlagFingerprint : Fingerprint(
-    strings = listOf("MailAndroidUpsellingSidebar"),
+private val SIDEBAR_UPSELLING_LABELS = listOf(
+    "drawer_upgrade_plus",
+    "drawer_upgrade_unlimited",
+    "drawer_upgrade_plus_black_friday",
+    "drawer_upgrade_plus_spring_sale",
+    "drawer_upgrade_plus_summer_sale",
+)
+
+internal val sidebarUpsellingRowFingerprints = SIDEBAR_UPSELLING_LABELS.map { label ->
+    Fingerprint(
+        returnType = "V",
+        filters = listOf(
+            resourceLiteral(ResourceType.STRING, label, exceptionIfResourceNotFound = false),
+        ),
+    )
+}
+
+internal object UpgradeUpsellingButtonFingerprint : Fingerprint(
+    returnType = "V",
+    filters = listOf(
+        resourceLiteral(ResourceType.STRING, "upselling_button_item_content_description"),
+    ),
 )
