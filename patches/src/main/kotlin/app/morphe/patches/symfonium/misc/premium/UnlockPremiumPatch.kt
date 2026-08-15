52dcd24a62042f59f0339d67e3c1d556b499644c
hxreborn <hxreborn@duck.com>
2026-08-15 09:39:39 +0200

    feat(Symfonium - Unlock premium): add patch
    Signed-off-by: hxreborn <hxreborn@duck.com>

---
+++ patches/src/main/kotlin/app/morphe/patches/symfonium/misc/premium/UnlockPremiumPatch.kt
@@ -0,0 +1,41 @@
+/*
+ * SPDX-FileCopyrightText: 2026 hxreborn
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+package app.morphe.patches.symfonium.misc.premium
+
+import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
+import app.morphe.patcher.patch.bytecodePatch
+import app.morphe.patches.shared.compat.AppCompatibilities
+import app.morphe.util.indexOfFirstInstructionOrThrow
+import app.morphe.util.matchSingle
+import app.morphe.util.returnEarly
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.instruction.WideLiteralInstruction
+
+@Suppress("unused")
+val unlockPremiumPatch = bytecodePatch(
+    name = "Unlock premium",
+    description = "Unlocks all premium features.",
+) {
+    compatibleWith(AppCompatibilities.SYMFONIUM)
+
+    execute {
+        val licenseManager = LicenseKeyCheckFingerprint.matchSingle().classDef
+
+        val isLicensed = licenseManager.methods.single {
+            it.returnType == "Z" && it.parameters.isEmpty()
+        }
+        val licensedState = isLicensed.implementation!!.instructions
+            .take(isLicensed.indexOfFirstInstructionOrThrow(Opcode.CMP_LONG))
+            .filterIsInstance<WideLiteralInstruction>()
+            .last()
+            .wideLiteral
+
+        licenseManager.methods
+            .single { it.returnType == "V" && it.parameters.singleOrNull()?.type == "J" }
+            .addInstructions(0, "const-wide p1, ${licensedState}L")
+
+        NativeVerdictHandlerFingerprint.matchSingle().method.returnEarly()
+    }
+}
