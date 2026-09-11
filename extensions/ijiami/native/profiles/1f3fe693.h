aa8167df54bcb82a6e6b7493a9767f2c01728131
hxreborn <hxreborn@duck.com>
2026-09-11 19:56:27 +0200

    chore: use the full GPL notice in the native and hx package sources
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- extensions/ijiami/native/profiles/1f3fe693.h
+++ extensions/ijiami/native/profiles/1f3fe693.h
@@ -1,6 +1,17 @@
 /*
- * SPDX-FileCopyrightText: 2026 hxreborn
- * SPDX-License-Identifier: GPL-3.0-only
+ * Copyright (C) 2026 hxreborn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 #define IMAGE_MIN_SIZE 0x39000
 #define ANCHOR_A_OFFSET 0x1218
