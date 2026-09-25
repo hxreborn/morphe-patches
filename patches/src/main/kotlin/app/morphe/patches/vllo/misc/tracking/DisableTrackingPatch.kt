594e3367971485e9a23aba8ed5da91e120908659
hxreborn <hxreborn@duck.com>
2026-09-25 19:36:02 +0200

    refactor: move analytics collection patch to shared
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- patches/src/main/kotlin/app/morphe/patches/vllo/misc/tracking/DisableTrackingPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/vllo/misc/tracking/DisableTrackingPatch.kt
@@ -5,44 +5,13 @@
 package app.morphe.patches.vllo.misc.tracking
 
 import app.morphe.patcher.patch.bytecodePatch
-import app.morphe.patcher.patch.resourcePatch
 import app.morphe.patches.shared.compat.AppCompatibilities
+import app.morphe.patches.shared.misc.analytics.disableAnalyticsCollectionPatch
 import app.morphe.patches.shared.misc.pairip.removePairipProtectionPatch
 import app.morphe.patches.shared.misc.pairip.removePairipVirtualizationPatch
 import app.morphe.patches.vllo.requireArm64Delta
 import app.morphe.util.matchSingle
 import app.morphe.util.returnEarly
-import org.w3c.dom.Element
-
-private val disabledCollectionFlags = listOf(
-    "firebase_analytics_collection_enabled",
-    "google_analytics_adid_collection_enabled",
-    "google_analytics_ssaid_collection_enabled",
-    "com.facebook.sdk.AutoLogAppEventsEnabled",
-    "com.facebook.sdk.AdvertiserIDCollectionEnabled",
-)
-
-private val disableAnalyticsCollectionPatch = resourcePatch {
-    execute {
-        document("AndroidManifest.xml").use { document ->
-            val application = document.getElementsByTagName("application").item(0)
-            val declared = buildSet {
-                val existing = document.getElementsByTagName("meta-data")
-                for (index in 0 until existing.length) {
-                    add((existing.item(index) as Element).getAttribute("android:name"))
-                }
-            }
-
-            disabledCollectionFlags.forEach { name ->
-                if (name in declared) return@forEach
-                val metadata = document.createElement("meta-data")
-                metadata.setAttribute("android:name", name)
-                metadata.setAttribute("android:value", "false")
-                application.appendChild(metadata)
-            }
-        }
-    }
-}
 
 @Suppress("unused")
 val disableTrackingPatch = bytecodePatch(
