80dbf398de25c8a9b88956f01d33ba60cb4d4f4b
hxreborn <hxreborn@duck.com>
2026-09-26 12:39:21 +0200

    feat(Proton VPN - Unlock LAN connections): allow local network access on free plans
    Signed-off-by: hxreborn <hxreborn@duck.com>

---
+++ patches/src/main/kotlin/app/morphe/patches/protonvpn/misc/lan/Fingerprints.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2026 Hoo-dles
+ * Copyright (C) 2026 hxreborn
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Ported from hoo-dles/morphe-patches:
+ * https://github.com/hoo-dles/morphe-patches/commit/3ad54cf13090739041b9f74c64c95e9994a0d980
+ * Commit 3ad54cf13090739041b9f74c64c95e9994a0d980 (2026-07-27),
+ * patches/src/main/kotlin/hoodles/morphe/patches/protonvpn/lan/Fingerprints.kt
+ */
+package app.morphe.patches.protonvpn.misc.lan
+
+import app.morphe.patcher.Fingerprint
+import app.morphe.patches.protonvpn.misc.restrictions.RestrictionGuardFingerprint
+
+internal object LanConnectionsViewStateFingerprint : Fingerprint(
+    definingClass = "Lcom/protonvpn/android/redesign/settings/ui/SettingsViewModel\$SettingViewState\$LanConnections;",
+    name = "<init>",
+    parameters = listOf(
+        "Z",
+        "Ljava/lang/Boolean;",
+        "Z",
+        "Lcom/protonvpn/android/redesign/vpn/ui/ConnectIntentPrimaryLabel\$Profile;",
+    ),
+)
+
+internal object LanConnectionsRestrictionFingerprint : RestrictionGuardFingerprint("getLanConnections")
---
+++ patches/src/main/kotlin/app/morphe/patches/protonvpn/misc/lan/UnlockLanConnectionsPatch.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2026 Hoo-dles
+ * Copyright (C) 2026 hxreborn
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Ported from hoo-dles/morphe-patches:
+ * https://github.com/hoo-dles/morphe-patches/commit/3ad54cf13090739041b9f74c64c95e9994a0d980
+ * Commit 3ad54cf13090739041b9f74c64c95e9994a0d980 (2026-07-27),
+ * patches/src/main/kotlin/hoodles/morphe/patches/protonvpn/lan/UnlockLanConnectionsPatch.kt
+ */
+package app.morphe.patches.protonvpn.misc.lan
+
+import app.morphe.patcher.patch.bytecodePatch
+import app.morphe.patches.protonvpn.misc.restrictions.unlockUserSetting
+import app.morphe.patches.shared.compat.AppCompatibilities
+
+@Suppress("unused")
+val unlockLanConnectionsPatch = bytecodePatch(
+    name = "Unlock LAN connections",
+    description = "Unlocks LAN connections on free plans.",
+) {
+    compatibleWith(AppCompatibilities.PROTON_VPN)
+
+    execute {
+        unlockUserSetting(
+            LanConnectionsViewStateFingerprint,
+            freeUserParameter = 3,
+            LanConnectionsRestrictionFingerprint,
+        )
+    }
+}
